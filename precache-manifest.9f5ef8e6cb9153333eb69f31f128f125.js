2919a78dac65b0594cdebe7501a7cbaeb03e7b1c
Moiz Khan <mmoizkhan780@gmail.com>
2020-11-28 23:41:52 +0500

    Updates

---
+++ precache-manifest.9f5ef8e6cb9153333eb69f31f128f125.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922bb0140fd698fb170a33c3c6ff9b19",
+    "url": "/Front_Ecommerce_App/index.html"
+  },
+  {
+    "revision": "79e2851444792dbae10d",
+    "url": "/Front_Ecommerce_App/static/css/main.74462bc5.chunk.css"
+  },
+  {
+    "revision": "91712656b993d79ba611",
+    "url": "/Front_Ecommerce_App/static/js/2.4c6847e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Front_Ecommerce_App/static/js/2.4c6847e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e2851444792dbae10d",
+    "url": "/Front_Ecommerce_App/static/js/main.bd7c400d.chunk.js"
+  },
+  {
+    "revision": "8d760fdb6d8cf3fa984d",
+    "url": "/Front_Ecommerce_App/static/js/runtime-main.c24b7b7b.js"
+  },
+  {
+    "revision": "99bfcf360dd5b0e9ce5789057d9f8e92",
+    "url": "/Front_Ecommerce_App/static/media/banner.99bfcf36.jpg"
+  },
+  {
+    "revision": "5550049a5f2bcd29d773f149db86ad58",
+    "url": "/Front_Ecommerce_App/static/media/s1.5550049a.jpg"
+  },
+  {
+    "revision": "825c69bafa4810b4afcecdad78e0cf62",
+    "url": "/Front_Ecommerce_App/static/media/s4.825c69ba.jpg"
+  },
+  {
+    "revision": "5550049a5f2bcd29d773f149db86ad58",
+    "url": "/Front_Ecommerce_App/static/media/s5.5550049a.jpg"
+  }
+]);
